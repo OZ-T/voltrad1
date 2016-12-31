d03ab27b1618ca6444b5483c7dbfd0aaaa6924ba
David <delucas.david@gmail.com>
2016-12-31 09:48:20 +0100

    First version REPL for voltrad1

--- volibutils/orders_methods.py
+++ volibutils/orders_methods.py
@@ -11,9 +11,17 @@ from swigibpy import Contract as IBcontract
 from swigibpy import Order as IBOrder
 import time
 
-globalconf = config.GlobalConfig(level=logger.ERROR)
-log = globalconf.log
-client = ib.IBClient(globalconf)
+def init_func():
+    globalconf = config.GlobalConfig(level=logger.ERROR)
+    log = globalconf.log
+    client = ib.IBClient(globalconf)
+    clientid1 = int(globalconf.config['ib_api']['clientid_orders'])
+    client.connect(clientid1=clientid1)
+
+    return client , log
+
+def end_func(client):
+    client.disconnect()
 
 def bs_resolve(x):
     if x<0:
@@ -23,7 +31,19 @@ def bs_resolve(x):
     if x==0:
         raise Exception("trying to trade with zero")
 
-def place_plain_order(client,expiry,symbol,right,strike,orderType,quantity,lmtPrice):
+def place_plain_order(expiry,symbol,right,strike,orderType,quantity,lmtPrice):
+    """
+    Place a sinlge option order
+    :param expiry:
+    :param symbol:
+    :param right:
+    :param strike:
+    :param orderType:
+    :param quantity:
+    :param lmtPrice:
+    :return:
+    """
+    client, log = init_func()
     log.info("placing order ")
     ibcontract = IBcontract()
     ibcontract.secType = "FOP"
@@ -45,11 +65,11 @@ def place_plain_order(client,expiry,symbol,right,strike,orderType,quantity,lmtPr
 
     orderid1 = client.place_new_IB_order(ibcontract, iborder, orderid=None)
     log.info("orderid [%s] " % (str(orderid1)))
+    end_func(client=client)
 
-
-def place_or_modif_spread_order(client,expiry,symbol,right,strike_l,strike_s,orderType,quantity,lmtPrice,orderId):
+def place_or_modif_spread_order(expiry,symbol,right,strike_l,strike_s,orderType,quantity,lmtPrice,orderId):
     """
-    Place new order or modify existing order
+    Place new option spread order or modify existing order
 
     :param client:
     :param expiry:
@@ -62,6 +82,7 @@ def place_or_modif_spread_order(client,expiry,symbol,right,strike_l,strike_s,ord
     :param lmtPrice:
     :return:
     """
+    client, log = init_func()
     log.info("placing order ")
 
     underl = {
@@ -103,17 +124,22 @@ def place_or_modif_spread_order(client,expiry,symbol,right,strike_l,strike_s,ord
     orderid1 = client.place_new_IB_order(ibcontract, iborder, orderid=orderId)
 
     print("orderid [%s] " % (str(orderid1)))
+    end_func(client=client)
 
-
-def list_open_orders(client):
+def list_open_orders():
+    """
+    List all currently open orders for this client
+    :return:
+    """
+    client, log = init_func()
     log.info("list orders ")
 
     order_structure = client.get_open_orders()
     for idx, x in order_structure.iteritems():
         print("[%s]" % (str(x)))
+    end_func(client=client)
 
-def modify_open_order(client,orderId):
-    # http://interactivebrokers.github.io/tws-api/modifying_orders.html#gsc.tab=0
+def modify_open_order(orderId):
     """
     Modification of an open order through the API can be achieved by the same client which placed the original order.
     In the case of orders placed manually in TWS, the order can be modified by the client with ID 0.
@@ -123,18 +149,38 @@ def modify_open_order(client,orderId):
     IBApi.Order.OrderId of the original. It is not generally recommended to try to change order parameters other than
     the order price and order size. To change other parameters, it might be preferable to cancel the original order
     and place a new order.
+    """
+    # http://interactivebrokers.github.io/tws-api/modifying_orders.html#gsc.tab=0
+    client, log = init_func()
+    end_func(client=client)
 
+def cancel_open_order():
     """
 
-    pass
+    :return:
+    """
+    client, log = init_func()
+    end_func(client=client)
 
-def cancel_open_order(client):
-    pass
 
-def cancel_all_open_orders(client):
-    pass
+def cancel_all_open_orders():
+    """
 
-def list_prices_before_trade(client,symbol,expiry,query):
+    :return:
+    """
+    client, log = init_func()
+    end_func(client=client)
+
+
+def list_prices_before_trade(symbol,expiry,query):
+    """
+    List prices before trade
+    :param symbol:
+    :param expiry:
+    :param query:
+    :return:
+    """
+    client, log = init_func()
     ctrt = {}
     for idx, x in enumerate(query):
         ctrt[idx] = RequestOptionData(symbol,'FOP',expiry,float(x[1:]),x[:1],'50','GLOBEX','USD',idx)
@@ -145,8 +191,18 @@ def list_prices_before_trade(client,symbol,expiry,query):
         subset_dic = {k: req1.get_in_data()[k] for k in ('strike', 'right', 'expiry','symbol')}
         subset_dic2 = {k: req1.get_out_data()[id][k] for k in ('bidPrice', 'bidSize', 'askPrice', 'askSize') }
         print subset_dic,subset_dic2
+    end_func(client=client)
 
 def list_spread_prices_before_trade(client,symbol,expiry,query):
+    """
+    List option spread prices before trade
+    :param client:
+    :param symbol:
+    :param expiry:
+    :param query:
+    :return:
+    """
+    client, log = init_func()
     underl = {}
 
     for idx, x in enumerate(query):
@@ -174,25 +230,20 @@ def list_spread_prices_before_trade(client,symbol,expiry,query):
 
     ctrt = {}
     ctrt[100] = RequestOptionData(symbol,'FOP',expiry,float(x[1:]),x[:1],'50','GLOBEX','USD',100,comboLegs=None,contract=ibcontract)
-
-
     ctrt_prc = client.getMktData(ctrt)
     #log.info("[%s]" % (str(ctrt_prc)))
     for id, req1 in ctrt_prc.iteritems():
         subset_dic = {k: req1.get_in_data()[k] for k in ('secType','symbol')}
         subset_dic2 = {k: req1.get_out_data()[id][k] for k in ('bidPrice', 'bidSize', 'askPrice', 'askSize') }
         print subset_dic,subset_dic2
-
-
+#
+    end_func(client=client)
 
 if __name__=="__main__":
-    clientid1 = int(globalconf.config['ib_api']['clientid_orders'])
-    client.connect(clientid1=clientid1)
-    #list_prices_before_trade(client=client,symbol="ES",expiry="20170120",query=['C2300.0','C2350.0','P2100.0','P2150.0'])
-    list_spread_prices_before_trade(client=client,symbol="ES",expiry="20170120",query=['C2300.0','C2350.0'])
-    #place_plain_order(client=client,expiry="20170120",symbol="ES",right="C",strike=2200.0,orderType="LMT",quantity=2,lmtPrice=5.0)
-    place_or_modif_spread_order(client=client,expiry="20170120",symbol="ES",right="C",strike_l=2300.0,
+    #list_prices_before_trade(symbol="ES",expiry="20170120",query=['C2300.0','C2350.0','P2100.0','P2150.0'])
+    #list_spread_prices_before_trade(symbol="ES",expiry="20170120",query=['C2300.0','C2350.0'])
+    #place_plain_order(expiry="20170120",symbol="ES",right="C",strike=2200.0,orderType="LMT",quantity=2,lmtPrice=5.0)
+    place_or_modif_spread_order(expiry="20170120",symbol="ES",right="C",strike_l=2300.0,
                        strike_s=2350.0,orderType="LMT",quantity=-1,lmtPrice=3.7,orderId=None)
-    list_open_orders(client=client)
+    #list_open_orders()
 
-    client.disconnect()
