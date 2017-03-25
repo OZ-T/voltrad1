d402d8ecdc34cdda2447804551b96a66bc309b23
David <delucas.david@gmail.com>
2017-03-25 06:41:50 +0100

    technical changes and fixes

--- shell/run_backup.sh
+++ shell/run_backup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd /tmp
 DATE=`date +%y%m%d` 
-nohup tar cf /home/david/Dropbox/proyectos/backup_voltrad1/${DATE}.tar.gz --use-compress-prog=pbzip2 /data/voltrad1/ >> /var/log/voltrad1/backup_${DATE}.log 2>&1 &
+nohup tar cf /home/david/backup_voltrad1/${DATE}.tar.gz --use-compress-prog=pbzip2 /data/voltrad1/ >> /var/log/voltrad1/backup_${DATE}.log 2>&1 &
 
 
 
---
+++ volatility-trading/volatility/models/GarmanKlass.py
@@ -0,0 +1,27 @@
+import math
+import datetime
+
+import pandas
+import numpy as np
+
+import data
+
+def get_estimator(symbol, start, end, window=30, clean=True):
+    
+    prices = data.get_data(symbol, start, end)
+    
+    log_hl = (prices['High'] / prices['Low']).apply(np.log)
+    log_co = (prices['Close'] / prices['Open']).apply(np.log)
+
+    rs = 0.5 * log_hl**2 - (2*math.log(2)-1) * log_co**2
+    
+    def f(v):
+        return math.sqrt(252 * v.mean())
+    
+    result = rs.rolling(window=window, center=False).apply(func=f)
+    result[:window-1] = np.nan
+    
+    if clean:
+        return result.dropna()
+    else:
+        return result
