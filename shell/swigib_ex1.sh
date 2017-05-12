c1d952ffd3a63e31f69c65bc58ec08ee6c2bb0f1
David <delucas.david@gmail.com>
2017-05-12 07:25:37 +0200

    Fix testing IB

--- shell/swigib_ex1.sh
+++ shell/swigib_ex1.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd /tmp
-export PYTHONPATH=/home/david/python
+export PYTHONPATH=/home/david/voltrad1
 DATE=`date +%d-%m-%y` 
-nohup /home/david/anaconda2/bin/python /home/david/python/sysIB/swigibpy_example1.py >> /var/log/voltrad1/ib_chain_opt_${DATE}.log 2>&1 &
+nohup /home/david/anaconda2/bin/python /home/david/voltrad1/test/swigibpy_example1.py >> /var/log/voltrad1/ib_test_${DATE}.log 2>&1 &
