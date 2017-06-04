3e8d7af42ccf502cbf4a7cf6c63740b85538c851
David <delucas.david@gmail.com>
2017-06-04 19:52:13 +0200

    Fix p commands for Python 3

--- shell/run_ib_orders.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd /tmp
-export PYTHONPATH=/home/david/voltrad1
-DATE=`date +%d-%m-%y` 
-nohup /home/david/anaconda2/bin/python /home/david/voltrad1/volblotter/vol_grab_orders.py >> /var/log/voltrad1/ib_orders_${DATE}.log 2>&1 &
