353b173350674bece1bd76f9ef34a078bf6c99f7
David <delucas.david@gmail.com>
2017-06-11 12:09:05 +0200

    Replacing hdf5 with sqllite and  migration

--- shell/run_backup.sh
+++ shell/run_backup.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 BK_DATE=`date +%Y_%m_%d__%H_%M_%S`
 cd /mnt/volume-fra1-01/voltrad1
-nohup tar cf /mnt/volume-fra1-01/backup/${BK_DATE}.tar.gz --use-compress-prog=pbzip2 *.db >> /var/log/voltrad1/backup_${BK_DATE}.log 2>&1 &
+nohup tar cf /mnt/volume-fra1-01/backup/${BK_DATE}.tar.bz2 --use-compress-prog=pbzip2 *.db >> /var/log/voltrad1/backup_${BK_DATE}.log 2>&1 &
+
+# to un compress:
+# 1.- bzip2 -vd 2017_06_11__10_53_22.tar.bz2
+# 2.- tar -xvf 2017_06_11__10_53_22.tar
 
 
 
