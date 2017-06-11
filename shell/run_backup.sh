6ccb96821cc7ade8a82825c4f3d3fb2bca322dc5
David <delucas.david@gmail.com>
2017-06-11 19:07:00 +0200

    change location of backup directory to a different filesystem

--- shell/run_backup.sh
+++ shell/run_backup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 BK_DATE=`date +%Y_%m_%d__%H_%M_%S`
 cd /mnt/volume-fra1-01/voltrad1
-nohup tar cf /mnt/volume-fra1-01/backup/${BK_DATE}.tar.bz2 --use-compress-prog=pbzip2 *.db >> /var/log/voltrad1/backup_${BK_DATE}.log 2>&1 &
+nohup tar cf /data/backup_voltrad1/${BK_DATE}.tar.bz2 --use-compress-prog=pbzip2 *.db >> /var/log/voltrad1/backup_${BK_DATE}.log 2>&1 &
 
 # to un compress:
 # 1.- bzip2 -vd 2017_06_11__10_53_22.tar.bz2
