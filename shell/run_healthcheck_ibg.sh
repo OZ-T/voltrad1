f8efcb49ea8be6b10443319520c0b2b4cba02bef
David <delucas.david@gmail.com>
2017-06-17 10:05:00 +0200

    health check service IBG

--- shell/run_healthcheck_ibg.sh
+++ shell/run_healthcheck_ibg.sh
@@ -2,4 +2,13 @@
 cd /tmp
 export PYTHONPATH=/home/david/voltrad1
 DATE=`date +%d-%m-%y` 
-/home/david/anaconda3/bin/python /home/david/voltrad1/tests/swigibpy_example1.py 
+#/home/david/anaconda3/bin/python /home/david/voltrad1/tests/swigibpy_example1.py 
+
+if [[ $(( /home/david/anaconda3/bin/python /home/david/voltrad1/tests/swigibpy_example1.py ) 2>&1 | grep "Error" ) ]]; then
+    echo "Something wrong with IBG. Restarting the service ..."
+    /home/david/shell/gw.sh stop sim 
+    /home/david/shell/gw.sh stop sim 
+    /home/david/shell/gw.sh start sim 
+else
+    echo "IBG is answering request. Nothing to do for now."
+fi
