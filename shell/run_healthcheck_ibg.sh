1ab94b700c0c920c32fb27aa09330b1db1ee6630
David <delucas.david@gmail.com>
2017-06-17 09:55:06 +0200

    healthcheck script

--- shell/run_healthcheck_ibg.sh
+++ shell/run_healthcheck_ibg.sh
@@ -2,4 +2,4 @@
 cd /tmp
 export PYTHONPATH=/home/david/voltrad1
 DATE=`date +%d-%m-%y` 
-nohup /home/david/anaconda2/bin/python /home/david/voltrad1/test/swigibpy_example1.py >> /var/log/voltrad1/ib_test_${DATE}.log 2>&1 &
+/home/david/anaconda3/bin/python /home/david/voltrad1/tests/swigibpy_example1.py 
