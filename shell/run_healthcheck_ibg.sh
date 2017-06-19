7c7e7f3a7e7446affb69ef30da658123c8430cfd
David <delucas.david@gmail.com>
2017-06-19 07:09:15 +0200

    added datetime to log for IGW healthcheck bash script

--- shell/run_healthcheck_ibg.sh
+++ shell/run_healthcheck_ibg.sh
@@ -3,12 +3,12 @@ cd /tmp
 export PYTHONPATH=/home/david/voltrad1
 DATE=`date +%d-%m-%y` 
 #/home/david/anaconda3/bin/python /home/david/voltrad1/tests/swigibpy_example1.py 
-
+NOW1=`date +%d-%m-%y\ %H:%M:%S`
 if [[ $(( /home/david/anaconda3/bin/python /home/david/voltrad1/tests/swigibpy_example1.py ) 2>&1 | grep "Error" ) ]]; then
-    echo "Something wrong with IBG. Restarting the service ..."
+    echo $NOW1 " Something wrong with IBG. Restarting the service ..."
     /home/david/shell/gw.sh stop sim 
     /home/david/shell/gw.sh stop sim 
     /home/david/shell/gw.sh start sim 
 else
-    echo "IBG is answering request. Nothing to do for now."
+    echo $NOW1 "IBG is answering request. Nothing to do for now."
 fi
