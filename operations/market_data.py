74df82629520ee9f0eb44114878ed37e3f3eaf57
David <delucas.david@gmail.com>
2017-06-24 17:52:25 +0200

    refactoring

--- operations/market_data.py
+++ operations/market_data.py
@@ -12,7 +12,9 @@ import time
 import pandas_datareader.data as web
 from pandas_datareader._utils import RemoteDataError
 import sys
-
+from volibutils.sync_client import IBClient
+from volibutils.RequestUnderlyingData import RequestUnderlyingData
+from time import sleep
 
 def get_market_db_file(globalconf,db_type,expiry):
     if db_type == "optchain_ib":
@@ -87,7 +89,95 @@ def write_market_data_to_sqllite(globalconf, log, dataframe, db_type):
             joe.to_sql(name, store, if_exists='append')
         store.close()
 
+def store_underlying_ib_to_db():
+    log=logger("historical data loader")
+    log.info("Getting historical underlying data from IB ... ")
+    globalconf = config.GlobalConfig()
+    path = globalconf.config['paths']['data_folder']
+    underly_def = globalconf.get_tickers_historical_ib()
+    client = IBClient(globalconf)
+    clientid1 = int(globalconf.config['ib_api']['clientid_data'])
+    client.connect(clientid1=clientid1)
 
+    dt_now=dt.datetime.now()
+    endDateTime =  dt_now.strftime('%Y%m%d %H:%M:%S')
+    # lo mas que se puede pedir para barras de 30 min es un mes historico
+    # barSizeSetting = "30 mins"
+    barSizeSetting = "1 min"
+    whatToShow = "TRADES"
+    useRTH = 1
+    formatDate = 1
+    wait_secs = 40
+
+    db_file = get_market_db_file(globalconf=globalconf, db_type="underl_ib_hist", expiry="NONE")
+    store = sqlite3.connect(path + db_file)
+
+    for index, row_req in underly_def.iterrows():
+        log.info("underl=[%s] [%s] [%s] [%d] [%s] [%s] [%s] [%s] [%d]"
+                        % ( str(row_req['symbol']), str(row_req['underl_type']),
+                            str(row_req['underl_expiry']), 0, '', '',
+                            str(row_req['underl_ex']), str(row_req['underl_curr']), int(index) ) )
+
+        ticker = RequestUnderlyingData(str(row_req['symbol']), str(row_req['underl_type']), str(row_req['underl_expiry']), 0, '', '',
+                                       str(row_req['underl_ex']), str(row_req['underl_curr']), int(index))
+        symbol_expiry = "/" + str(row_req['symbol']) + "/" + str(row_req['underl_expiry'])
+
+        sql = "SELECT count(*) as count1 FROM sqlite_master WHERE type='table' AND name='" + str(row_req['symbol']) + "';"
+        tbl_exist = pd.read_sql_query(sql, store)['count1'][0]
+        log.info("Does this symbol has a table in sqlite? " + str(tbl_exist))
+
+        if tbl_exist:
+            if long(row_req['underl_expiry']) > 0:
+                sql = "SELECT MAX(DATE) as max1 FROM " + str(row_req['symbol']) +" WHERE EXPIRY = '" + symbol_expiry + "' ;"
+            else:
+                sql = "SELECT MAX(DATE) as max1 FROM " + str(row_req['symbol']) + " ;"
+
+            last_date = pd.read_sql_query( sql , store)['max1'][0]
+            log.info("Last date in data store for symbol " + symbol_expiry + " is " + str(last_date) )
+            if last_date is not None:
+                last_record_stored = dt.datetime.strptime(str(last_date), '%Y%m%d %H:%M:%S')
+                # no se debe usar .seconds que da respuesta incorrecta debe usarse .total_seconds()
+                # days= int(round( (dt_now - last_record_stored).total_seconds() / 60 / 60 / 24  ,0))
+                # lo anterior no sirve porque debe considerarse la diferencia en business days
+                # days = np.busday_count(last_record_stored.date(), dt_now.date())
+                bh = utils.BusinessHours(last_record_stored, dt_now, worktiming=[15, 21], weekends=[6, 7])
+                days = bh.getdays()
+                durationStr = str(days) + " D"
+            else:
+                last_record_stored = 0
+                durationStr = "30 D"
+                barSizeSetting = "30 mins"
+
+            if str(row_req['symbol']) in ['NDX','SPX','VIX']:
+                barSizeSetting = "30 mins"
+        else:
+            last_record_stored = 0
+            durationStr = "30 D"
+            barSizeSetting = "30 mins"
+
+        log.info( "last_record_stored=[%s] endDateTime=[%s] durationStr=[%s] barSizeSetting=[%s]"
+                  % ( str(last_record_stored), str(endDateTime) , durationStr, barSizeSetting) )
+
+        historicallist = client.get_historical(ticker, endDateTime, durationStr, barSizeSetting,whatToShow, useRTH,formatDate)
+        dataframe = pd.DataFrame()
+        if historicallist:
+            for reqId, request in historicallist.items():
+                for date, row in request.items():
+                    temp1 = pd.DataFrame(row, index=[0])
+                    temp1['symbol'] = str(row_req['symbol'])
+                    temp1['expiry'] = str(row_req['underl_expiry'])
+                    temp1['type'] = str(row_req['underl_type'])
+                    temp1['load_dttm'] = endDateTime
+                    dataframe = dataframe.append(temp1.reset_index().drop('index', 1))
+            dataframe = dataframe.sort_values(by=['date']).set_index('date')
+            log.info( "appending data to sqlite ...")
+            write_market_data_to_sqllite(globalconf, log, dataframe, "underl_ib_hist")
+
+        log.info("sleeping [%s] secs ..." % (str(wait_secs)))
+        sleep(wait_secs)
+
+    client.disconnect()
+    store.close()
 
 def store_optchain_yahoo_to_db():
     globalconf = config.GlobalConfig()
@@ -135,3 +225,5 @@ def CustomParser(data):
     return j1
 
 
+if __name__ == "__main__":
+    store_underlying_ib_to_db()
