5012e55820d250918d63381acd671e249a37e8ef
David <delucas.david@gmail.com>
2017-06-24 10:01:45 +0200

    refactoring

--- analytics/edgar_email.py
+++ analytics/edgar_email.py
@@ -10,6 +10,28 @@ import time
 import io
 import itertools as IT
 
+# -*- coding: UTF-8 -*-
+
+
+# http://developer.edgar-online.com/member/register
+
+# will expire two weeks after issue date so talk to us if you want to maintain your key!
+
+
+# use public URL instead, like:
+
+# https://www.sec.gov/Archives/edgar/data/8670/0001225208-17-000059.txt
+
+# https://www.sec.gov/Archives/edgar/data/1053092/0000950103-17-000015.txt
+
+
+# el indice de todos los documentos y como cambiar la URL para acceder esta en:
+# https://www.sec.gov/edgar/searchedgar/accessing-edgar-data.htm
+
+#  --> https://www.sec.gov/Archives/edgar/daily-index/2017/QTR1/
+
+
+
 if __name__ == "__main__":
     CompanyNameList = []
     TickerList = []
